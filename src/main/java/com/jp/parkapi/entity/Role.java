b4a5db31217b216349e251bea2aa2517324ed5d9
João Paulo Lira de Almeida <joao.almeida@mv.com.br>
2023-12-04 13:00:32 -0300

    feat: Criada entidade Usuario e Enum Role para definir papel do usuário

---
+++ src/main/java/com/jp/parkapi/entity/Role.java
@@ -0,0 +1,5 @@
+package com.jp.parkapi.entity;
+
+public enum Role {
+    ROLE_ADMIN, ROLE_CLIENTE
+}
---
+++ src/main/java/com/jp/parkapi/entity/Usuario.java
@@ -0,0 +1,62 @@
+package com.jp.parkapi.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true, length = 100)
+    private String username;
+
+    @Column(name = "password", nullable = false, length = 200)
+    private String password;
+
+    @Enumerated(EnumType.STRING) //Transforma a constant em uma string para salvar no banco de dados
+    // EnumType.ORDINAL salva no banco pela posição declara, nesse exemplo
+    @Column(name = "role", nullable = false, length = 25)
+    private Role role;
+
+    @Column(name = "data_criacao")
+    private LocalDateTime dataCriacao;
+
+    @Column(name = "data_modificacao")
+    private LocalDateTime dataModificacao;
+
+    @Column(name = "criado_por")
+    private String criadoPor;
+
+    @Column(name = "modificado_por")
+    private String modificadoPor;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return id.equals(usuario.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "id=" + id +'}';
+    }
+}
