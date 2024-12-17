5d543d4ac902e477073bd7a3acb172691b23d081
João Paulo <jp.sport17@gmail.com>
2024-12-16 22:00:42 -0300

    test: Arquivo de dados para testar funcionalidades da entidade Cliente

---
+++ src/test/resources/sql/clientes/clientes-insert.sql
@@ -0,0 +1,19 @@
+insert into USUARIOS (id, username, password, role) values (100, 'ygor@email.com', '$2a$12$G0Fft5sfccGA9ZgjC4O0nOAn36K9jIP2.QrewAUIxruTUSlNKhcTi', 'ROLE_ADMIN');
+-- senha 123456
+
+insert into USUARIOS (id, username, password, role) values (101, 'tulio@email.com', '$2a$12$CZHXRYKYzW5XjmsX/x3t/eNvnl.TN7ertrpEmr8De9ebiQTQ92QV6', 'ROLE_CLIENTE');
+-- senha 235689
+
+insert into USUARIOS (id, username, password, role) values (102, 'dilma@email.com', '$2a$12$7e1c51cH0b7OiK0x3Xl.o.0ZsRGOe69AGNXzd215jA5UjSru/uaSu', 'ROLE_ADMIN');
+-- senha 124578
+
+insert into USUARIOS (id, username, password, role) values (103, 'maria@email.com', '$2a$12$7e1c51cH0b7OiK0x3Xl.o.0ZsRGOe69AGNXzd215jA5UjSru/uaSu', 'ROLE_CLIENTE');
+-- senha 124578
+
+insert into USUARIOS (id, username, password, role) values (104, 'tupac@email.com', '$2a$12$7e1c51cH0b7OiK0x3Xl.o.0ZsRGOe69AGNXzd215jA5UjSru/uaSu', 'ROLE_CLIENTE');
+-- senha 124578
+
+
+
+insert into CLIENTES (id, nome, cpf, id_usuario) values (101, 'Tulio', '39988750005', 101);
+insert into CLIENTES (id, nome, cpf, id_usuario) values (103, 'Maria', '87150818005', 103);
