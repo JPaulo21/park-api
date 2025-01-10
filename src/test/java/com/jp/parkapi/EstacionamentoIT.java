ebd2309a4d5932e913cb42049b754e72a501e3b3
João Paulo <jp.sport17@gmail.com>
2025-01-09 21:34:28 -0300

    test: fazer checkin no Estacionamento e suas exceções

--- src/test/java/com/jp/parkapi/EstacionamentoIT.java
+++ src/test/java/com/jp/parkapi/EstacionamentoIT.java
@@ -46,4 +46,154 @@ public class EstacionamentoIT {
                 .jsonPath("dataEntrada").exists()
                 .jsonPath("vagaCodigo").exists();
     }
+
+    @Test
+    public void criarCheckin_ComRoleCliente_RetornarErrorMessageStatus403(){
+        EstacionamentoCreateDto createDto = EstacionamentoCreateDto.builder()
+                .placa("WER-1111")
+                .marca("FIAT")
+                .modelo("UNO")
+                .cor("AZUL")
+                .clienteCpf("87150818005")
+                .build();
+
+        webTestClient
+                .post()
+                .uri("/api/v1/estacionamentos/check-in")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(webTestClient, "maria@email.com", "124578"))
+                .bodyValue(createDto)
+                .exchange()
+                .expectStatus().isForbidden()
+                .expectBody()
+                .jsonPath("status").isEqualTo("403")
+                .jsonPath("path").isEqualTo("/api/v1/estacionamentos/check-in")
+                .jsonPath("method").isEqualTo("POST");
+    }
+
+    @Test
+    public void criarCheckin_ComDadosInvalidos_RetornarErrorMessageStatus422(){
+        EstacionamentoCreateDto createDto = EstacionamentoCreateDto.builder()
+                .placa("")
+                .marca("")
+                .modelo("")
+                .cor("")
+                .clienteCpf("")
+                .build();
+
+        webTestClient
+                .post()
+                .uri("/api/v1/estacionamentos/check-in")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(webTestClient, "ygor@email.com", "123456"))
+                .bodyValue(createDto)
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody()
+                .jsonPath("status").isEqualTo("422")
+                .jsonPath("path").isEqualTo("/api/v1/estacionamentos/check-in")
+                .jsonPath("method").isEqualTo("POST");
+    }
+
+    @Test
+    public void criarCheckin_ComCpfInexistente_RetornarErrorMessageStatus404(){
+        EstacionamentoCreateDto createDto = EstacionamentoCreateDto.builder()
+                .placa("WER-1111")
+                .marca("FIAT")
+                .modelo("UNO")
+                .cor("AZUL")
+                .clienteCpf("43919081072")
+                .build();
+
+        webTestClient
+                .post()
+                .uri("/api/v1/estacionamentos/check-in")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(webTestClient, "ygor@email.com", "123456"))
+                .bodyValue(createDto)
+                .exchange()
+                .expectStatus().isNotFound()
+                .expectBody()
+                .jsonPath("status").isEqualTo("404")
+                .jsonPath("path").isEqualTo("/api/v1/estacionamentos/check-in")
+                .jsonPath("method").isEqualTo("POST");
+    }
+
+    @Sql(scripts = "/sql/estacionamentos/estacionamento-insert-vagas-ocupadas.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD) // Executando o script antes do método
+    @Sql(scripts = "/sql/estacionamentos/estacionamento-delete-vagas-ocupadas.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    @Test
+    public void criarCheckin_ComVagasOcupadas_RetornarErrorMessageStatus404(){
+        EstacionamentoCreateDto createDto = EstacionamentoCreateDto.builder()
+                .placa("WER-1111")
+                .marca("FIAT")
+                .modelo("UNO")
+                .cor("AZUL")
+                .clienteCpf("87150818005")
+                .build();
+
+        webTestClient
+                .post()
+                .uri("/api/v1/estacionamentos/check-in")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(webTestClient, "ygor@email.com", "123456"))
+                .bodyValue(createDto)
+                .exchange()
+                .expectStatus().isNotFound()
+                .expectBody()
+                .jsonPath("status").isEqualTo("404")
+                .jsonPath("path").isEqualTo("/api/v1/estacionamentos/check-in")
+                .jsonPath("method").isEqualTo("POST");
+    }
+
+    @Test
+    public void buscarCheckin_ComPerfilAdmin_RetornarDadosStatus200(){
+        webTestClient
+                .get()
+                .uri("/api/v1/estacionamentos/check-in/{recibo}", "20230313-101300")
+                .headers(JwtAuthentication.getHeaderAuthorization(webTestClient, "ygor@email.com", "123456"))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("placa").isEqualTo("FIT-1020")
+                .jsonPath("marca").isEqualTo("FIAT")
+                .jsonPath("modelo").isEqualTo("PALIO")
+                .jsonPath("cor").isEqualTo("VERDE")
+                .jsonPath("clienteCpf").isEqualTo("90287456021")
+                .jsonPath("recibo").isEqualTo("20230313-101300")
+                .jsonPath("dataEntrada").isEqualTo("13/03/2023 10:15:00")
+                .jsonPath("vagaCodigo").isEqualTo("A-01");
+    }
+
+    @Test
+    public void buscarCheckin_ComPerfilCliente_RetornarDadosStatus200(){
+        webTestClient
+                .get()
+                .uri("/api/v1/estacionamentos/check-in/{recibo}", "20230313-101300")
+                .headers(JwtAuthentication.getHeaderAuthorization(webTestClient, "tupac@email.com", "124578"))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("placa").isEqualTo("FIT-1020")
+                .jsonPath("marca").isEqualTo("FIAT")
+                .jsonPath("modelo").isEqualTo("PALIO")
+                .jsonPath("cor").isEqualTo("VERDE")
+                .jsonPath("clienteCpf").isEqualTo("90287456021")
+                .jsonPath("recibo").isEqualTo("20230313-101300")
+                .jsonPath("dataEntrada").isEqualTo("13/03/2023 10:15:00")
+                .jsonPath("vagaCodigo").isEqualTo("A-01");
+    }
+
+    @Test
+    public void buscarCheckin_ComReciboInexistente_RetornarErrorStatus404(){
+        webTestClient
+                .get()
+                .uri("/api/v1/estacionamentos/check-in/{recibo}", "20230313-999999")
+                .headers(JwtAuthentication.getHeaderAuthorization(webTestClient, "tupac@email.com", "124578"))
+                .exchange()
+                .expectStatus().isNotFound()
+                .expectBody()
+                .jsonPath("status").isEqualTo("404")
+                .jsonPath("path").isEqualTo("/api/v1/estacionamentos/check-in/20230313-999999")
+                .jsonPath("method").isEqualTo("GET");
+    }
 }
---
+++ src/test/resources/sql/estacionamentos/estacionamento-insert-vagas-ocupadas.sql
@@ -0,0 +1,40 @@
+insert into USUARIOS (id, username, password, role)
+    values (100, 'ygor@email.com', '$2a$12$G0Fft5sfccGA9ZgjC4O0nOAn36K9jIP2.QrewAUIxruTUSlNKhcTi', 'ROLE_ADMIN');
+-- senha 123456
+
+insert into USUARIOS (id, username, password, role)
+    values (101, 'tulio@email.com', '$2a$12$CZHXRYKYzW5XjmsX/x3t/eNvnl.TN7ertrpEmr8De9ebiQTQ92QV6', 'ROLE_CLIENTE');
+-- senha 235689
+
+insert into USUARIOS (id, username, password, role)
+    values (102, 'dilma@email.com', '$2a$12$7e1c51cH0b7OiK0x3Xl.o.0ZsRGOe69AGNXzd215jA5UjSru/uaSu', 'ROLE_ADMIN');
+-- senha 124578
+
+insert into USUARIOS (id, username, password, role)
+    values (103, 'maria@email.com', '$2a$12$7e1c51cH0b7OiK0x3Xl.o.0ZsRGOe69AGNXzd215jA5UjSru/uaSu', 'ROLE_CLIENTE');
+-- senha 124578
+
+insert into USUARIOS (id, username, password, role)
+    values (104, 'tupac@email.com', '$2a$12$7e1c51cH0b7OiK0x3Xl.o.0ZsRGOe69AGNXzd215jA5UjSru/uaSu', 'ROLE_CLIENTE');
+-- senha 124578
+
+
+insert into CLIENTES (id, nome, cpf, id_usuario) values (101, 'Tulio', '39988750005', 101);
+insert into CLIENTES (id, nome, cpf, id_usuario) values (103, 'Maria', '87150818005', 103);
+insert into CLIENTES (id, nome, cpf, id_usuario) values (104, 'Tupac Shakur', '90287456021', 104);
+
+
+insert into VAGAS (id, codigo, status) values (10, 'A-01', 'OCUPADA');
+insert into VAGAS (id, codigo, status) values (20, 'A-02', 'OCUPADA');
+insert into VAGAS (id, codigo, status) values (30, 'A-03', 'OCUPADA');
+insert into VAGAS (id, codigo, status) values (40, 'A-04', 'OCUPADA');
+
+
+insert into clientes_tem_vagas (numero_recibo, placa, marca, modelo, cor, data_entrada, id_cliente, id_vaga)
+    values ('20230313-101300', 'FIT-1020', 'FIAT', 'PALIO', 'VERDE', '2023-03-13 10:15:00', 104, 10);
+insert into clientes_tem_vagas (numero_recibo, placa, marca, modelo, cor, data_entrada, id_cliente, id_vaga)
+    values ('20230313-101400', 'SIE-1020', 'FIAT', 'SIENA', 'BRANCO', '2023-03-14 10:15:00', 103, 40);
+insert into clientes_tem_vagas (numero_recibo, placa, marca, modelo, cor, data_entrada, id_cliente, id_vaga)
+    values ('20230315-101500', 'FIT-1020', 'FIAT', 'PALIO', 'VERDE', '2023-03-14 10:15:00', 104, 30);
+insert into clientes_tem_vagas (numero_recibo, placa, marca, modelo, cor, data_entrada, id_cliente, id_vaga)
+    values ('20230315-101600', 'FIT-1030', 'FIAT', 'PALIO', 'AZUL', '2023-03-14 10:15:00', 104, 20);
